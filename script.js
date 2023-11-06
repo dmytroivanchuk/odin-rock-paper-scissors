14e44c8b5154b7421464384f2e51aa047b4921e3
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2023-11-06 18:05:56 +0200

    feat: write playRound function Complete steps 4 to 5.
    Steps must be completed in order to finish the project. Steps are
    provided in the "Project: Rock Paper Scissors" lesson of The Odin
    Project curriculum.

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
+/*
+Declare a function named getComputerChoice
+Declare a variable named choices with array, holding "Rock", "Paper" and "Scissors" text values
+Calculate a random array index based on array length and declare variable named random with calculation result
+Return a random text value from choices array using variable named random
+*/
 function getComputerChoice() {
-  const array = ["Rock", "Paper", "Scissors"];
-  const index = Math.floor(Math.random() * array.length);
-  const chosenText = array[index];
-  return chosenText;
+  const choices = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
+}
+
+/*
+Declare a function named playRound with two parameters: playerSelection and computerSelection
+Make playerSelection parameter case-insensitive
+If playerSelection is "rock" and computerSelection is "paper" return the following text: "You Lose! Paper beats Rock"
+If playerSelection is "rock" and computerSelection is "scissors" return the following text: "You Win! Rock beats Scissors"
+If playerSelection is "rock" and computerSelection is "rock" return the following text: "Draw! Both chose Rock"
+If playerSelection is "paper" and computerSelection is "rock" return the following text: "You Win! Paper beats Rock"
+If playerSelection is "paper" and computerSelection is "scissors" return the following text: "You Lose! Scissors beats Paper"
+If playerSelection is "paper" and computerSelection is "paper" return the following text: "Draw! Both chose Paper"
+If playerSelection is "scissors" and computerSelection is "rock" return the following text: "You Lose! Rock beats Scissors"
+If playerSelection is "scissors" and computerSelection is "paper" return the following text: "You Win! Scissors beats Paper"
+If playerSelection is "scissors" and computerSelection is "scissors" return the following text: "Draw! Both chose Scissors"
+If playerSelection don't match "rock", "paper" or "scissors" lowercase or uppercase text values return the following text: "Error! Please, type the correct element"
+*/
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "rock") {
+    return "Draw! Both chose Rock";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "Draw! Both chose Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return "Draw! Both chose Scissors";
+  } else {
+    return "Error! Please, type the correct element";
+  }
 }
